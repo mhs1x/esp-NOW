ff9b630f10ef36a6849428feeb5a50ba06c49eab
Marcus Henrique <marcushsxsilva@gmail.com>
2026-06-18 14:50:36 -0300

    Update main.c

--- esp now 1/main.c
+++ esp now 1/main.c
@@ -1,62 +1,101 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
-#include "comunicador.h"
+#include "esp_wifi.h"
+#include "esp_now.h"
+#include "esp_rom_sys.h"
+#include "nvs_flash.h" // biblioteca q ativa a memoria nvs para Wi-Fi/ESP-NOW
 
-// Feito por Marcus
-#define BOTAO 4
+// Pinos
+#define BTN_1 13
+#define BTN_2 12
+#define BTN_3 14
+#define BTN_4 27
+#define LED_LIGADO 2
+#define LED_LINK   18
+#define BUZZER     5
 
-// Definições de Tempo 
-#define UNIT_TIME_MS    200  // 1 ponto
-#define TEMPO_PONTO_MS  UNIT_TIME_MS
-#define TEMPO_TRACO_MS  (UNIT_TIME_MS * 3)
-#define ESPACO_SINAL    UNIT_TIME_MS       // Espaço entre . e - da mesma letra
-#define ESPACO_LETRA    (UNIT_TIME_MS * 3) // Espaço entre letras
-#define ESPACO_PALAVRA  (UNIT_TIME_MS * 7) // Espaço entre palavras
+typedef struct __attribute__((packed)) {
+    uint8_t botoes[4];
+} pacote_t;
 
-// Tabelas de Tradução
-const char* ALFABETO_MORSE[] = {
-    ".-",   "-...", "-.-.", "-..",  ".",    "..-.", "--.",  "....", "..", 
-    ".---", "-.-",  ".-..", "--",   "-.",   "---",  ".--.", "--.-", ".-.", 
-    "...",   "-",    "..-",   "...-", ".--",  "-..-", "-.--", "--.."};
+void visual() {
+    // leds e buzzer
+    gpio_set_direction(LED_LIGADO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_LINK, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUZZER, GPIO_MODE_OUTPUT);
 
-const char* NUMEROS_MORSE[] = {
-    "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+    // pull up dos botões
+    uint64_t mask = (1ULL<<BTN_1) | (1ULL<<BTN_2) | (1ULL<<BTN_3) | (1ULL<<BTN_4);
+    gpio_config_t io = { .pin_bit_mask = mask, .mode = GPIO_MODE_INPUT, .pull_up_en = GPIO_PULLUP_ENABLE };
+    gpio_config(&io);
 
+    // som inicial
+    gpio_set_level(LED_LIGADO, 1);
+    gpio_set_level(BUZZER, 1);
+    vTaskDelay(pdMS_TO_TICKS(200));
+    gpio_set_level(BUZZER, 0);
+}
 
-void transmitir_mensagem(const char* mensagem) {
-    for (int i = 0; i < strlen(mensagem); i++) {
-        char c = toupper((unsigned char)mensagem[i]);
-        
-        if (c >= 'A' && c <= 'Z') {
-            printf("Enviando letra: %c\n", c);
-            pulsar_morse(ALFABETO_MORSE[c - 'A']);
-            vTaskDelay(pdMS_TO_TICKS(ESPACO_LETRA)); 
-        } 
-        else if (c >= '0' && c <= '9') {
-            printf("Enviando numero: %c\n", c);
-            pulsar_morse(NUMEROS_MORSE[c - '0']);
-            vTaskDelay(pdMS_TO_TICKS(ESPACO_LETRA));}}}
+void app_main(void) {
+    // Inicialização do NVS 
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-void comunicador() {
-    printf("Sistema Transmissor Morse Pronto.\n");
+    visual();
+    
+    // Inicialização ESP-NOW
+    esp_netif_init();
+    esp_event_loop_create_default();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_start();
+    esp_now_init();
+
+    uint8_t peer_addr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    esp_now_peer_info_t peer = { .peer_addr = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF} };
+    esp_now_add_peer(&peer);
+
+    pacote_t dados = {0};
 
     while (1) {
-        // Se o botão for pressionado (Nível 0 devido ao Pull-up)
-        if (gpio_get_level(BOTAO) == 0) {
-            printf("\n--- Inicio da Transmissao ---\n");
-            transmitir_mensagem("SOS 1");
-            printf("--- Fim da Transmissao ---\n\n");
-            
-            // Aguarda o botão ser solto para não repetir sem parar
-            while(gpio_get_level(BOTAO) == 0) {
-                vTaskDelay(pdMS_TO_TICKS(50));
+        bool mudou = false;
+        bool apertou = false;   // clique do led link
+        
+        uint8_t estados_atuais[4] = {
+            !gpio_get_level(BTN_1), !gpio_get_level(BTN_2), 
+            !gpio_get_level(BTN_3), !gpio_get_level(BTN_4)
+        };
+
+        for(int i=0; i<4; i++) {
+            if(dados.botoes[i] != estados_atuais[i]) {
+                // Se o estado atual for 1 significa que o botão foi APERTADO
+                if(estados_atuais[i] == 1) {
+                    apertou = true;
+                }
+                dados.botoes[i] = estados_atuais[i];
+                mudou = true;
             }
         }
-        vTaskDelay(pdMS_TO_TICKS(100)); // Pequena pausa para economizar CPU
+
+        if (apertou) { // Pisca o LED imediatamente se qualquer botão foi apertado
+            gpio_set_level(LED_LINK, 1);
+            vTaskDelay(pdMS_TO_TICKS(50)); // Pisca rápido
+            gpio_set_level(LED_LINK, 0);
+        }
+
+        // Envia os dados se houve mudança (seja apertando ou soltando)
+        if (mudou) {
+            esp_now_send(peer_addr, (uint8_t*)&dados, sizeof(dados));
+        }
+        
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
